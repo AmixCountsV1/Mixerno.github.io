89d31e042827e4253b03203c0e84766401223af5
Mixerno <61512920+Mixerno@users.noreply.github.com>
2020-04-28 21:30:03 +0300

    Delete c/404code.js

--- c/404code.js
+++
@@ -1,7 +0,0 @@
-location.href = 'http://mixerno.github.io/';
-$(document).ready(function() {
-$('#a').click(function() {
-    location.href = 'http://mixerno.github.io/';
-    console.log("BUTTON2")
-  });
-});
