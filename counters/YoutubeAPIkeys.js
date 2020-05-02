cff9675fa2130e83d6736a236fc173ef05941890
Mixerno <61512920+Mixerno@users.noreply.github.com>
2020-05-03 00:58:21 +0300

    v3 lol

---
+++ counters/YoutubeAPIkeys.js
@@ -0,0 +1,19 @@
+var keyforsearch = [
+    'AIzaSyDiIbIzJMVnMfFNyp4pEJ4A0hJS5ecXwxc',
+    /*'',
+    '',
+    '',
+    '',*/
+];
+var keyforstats = [
+    'AIzaSyClejm10R131h8oioIoUGk47VVf2DsoVq0',
+    /*'',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',*/
+];
